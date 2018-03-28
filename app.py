f9347dd707938d52b24666252835e2492c708013
mireleschavezv <mireleschavezv@semantic-web.com>
2018-03-28 15:37:15 +0200

    added test data

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from auxApp import my_text_f
 conf = SparkConf().setAppName("Visapp1").setMaster(os.environ["SPARK_MASTER_URL"])
 sc = SparkContext(conf=conf)
 
-folderLocation = '../test_data/'
+folderLocation = 'test_data/'
 
 distData = sc.wholeTextFiles(folderLocation)
 coincidences = distData.map(my_text_f)
