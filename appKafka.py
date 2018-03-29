5edd3a66bbdb844bf682e62c2fb196f826646385
mireleschavezv <mireleschavezv@semantic-web.com>
2018-03-29 16:53:00 +0200

    test kafka consumer.py

--- appKafka.py
+++ appKafka.py
@@ -3,6 +3,12 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
+def count_words(x, debug=True):
+    sp = x[1].split()
+    if debug:
+        print("\n\n"+"->"+sp[0], len(x[1]), "\n")
+    return len(sp)
+
 sc = SparkContext(appName="PythonStreamingDirectKafkaWordCountRM")
 ssc = StreamingContext(sc, 3)
 kafkaStream = KafkaUtils.createStream(ssc,
@@ -11,7 +17,7 @@ kafkaStream = KafkaUtils.createStream(ssc,
                                       {'topic1':1})
 
 parsed = kafkaStream.map(lambda v: v.strip())
-counts = parsed.map(lambda x: len(x.split()))
+counts = parsed.map(count_words)
 print("\n\n\n\n XXXX \n\n\n\n")
 reduction = counts.reduce(lambda a, b: a + b)
 
