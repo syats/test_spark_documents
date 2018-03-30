4dc000a7fc09fe794eadcadd2cacdd665df3293e
mireleschavezv <mireleschavezv@semantic-web.com>
2018-03-30 11:56:45 +0200

    offsets 0

--- auxApp.py
+++ auxApp.py
@@ -1,7 +1,7 @@
 def count_words(x, debug=True):
     sp = x.split()
     if debug:
-        print("\n\n"+"->"+len(x), " :: ", x, "\n")
+        print("\n\n", "->", len(x), " :: ", x, "\n")
     return [(word, 1) for word in sp]
 
 
