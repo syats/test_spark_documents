4c9961de81dd0cb78ef602d4f2854e05c4238868
mireleschavezv <mireleschavezv@semantic-web.com>
2018-04-05 11:56:48 +0200

    using ppx

--- appKafka2.py
+++ appKafka2.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 from auxApp import count_words
 
 import sys
-
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils, TopicAndPartition
@@ -15,13 +14,14 @@ if __name__ == "__main__":
 
     sc = SparkContext(appName="PythonStreamingKafkaWordCount")
     ssc = StreamingContext(sc, 2)
-    fromOffsets = {TopicAndPartition(topic, 0): int(0)}
 
+    fromOffsets = {TopicAndPartition(topic, 0): int(0)}
     zkQuorum, topic = br, topic
     kvs = KafkaUtils.createDirectStream(ssc, [topic],
                                         {"metadata.broker.list": zkQuorum},
                                         fromOffsets=fromOffsets
                                         )
+
     lines = kvs.map(lambda x: x[1])
     # counts = lines.flatMap(lambda line: line.split(" "))\
     #     .map(lambda word: (word, 1)) \
@@ -30,10 +30,3 @@ if __name__ == "__main__":
     counts.pprint()
     ssc.start()
     ssc.awaitTermination()
-    #
-    # lines = kvs.map(lambda x: x[1])
-    # counts = lines.map(count_words).reduceByKey(lambda a, b: a+b)
-    # counts.pprint()
-    #
-    # ssc.start()
-    # ssc.awaitTermination()
--- auxApp.py
+++ auxApp.py
@@ -1,7 +1,27 @@
+import sys
+import os
+if os.path.exists('libs.zip'):
+    sys.path.insert(0, 'libs.zip')
+
+import pp_api.pp_calls as poolparty
+from configs.ppconfigs import *
+
+
+def extract_words(x, debug=True):
+    ppinstance = poolparty.PoolParty(server=pp_server, auth_data=auth_data)
+    r = ppinstance.extract(text=x, pid=pid)
+    cpts = ppinstance.get_cpts_from_response(r)
+    reply = []
+    for cpt in cpts:
+        reply.append((cpt['uri'], 1))
+    sp = x.split()
+    if debug:
+        print("\n\n", "->", len(x), " :: ", x, "\n")
+    return reply
+
+
 def count_words(x, debug=True):
     sp = x.split()
     if debug:
         print("\n\n", "->", len(x), " :: ", x, "\n")
     return [(word, 1) for word in sp]
-
-
